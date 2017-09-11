ff2ee39a2ff29397b9aa3cd8829b70cc780fea45
billyean <tristan.yim@gmail.com>
2017-09-11 15:34:15 -0700

    Add Tree Traverse

--- java/src/main/java/com/haibo/yan/algorithm/tree/Traversal.java
+++ java/src/main/java/com/haibo/yan/algorithm/tree/Traversal.java
@@ -30,26 +30,67 @@ public class Traversal {
 
     public List<Integer> inOrderTraverse(Node head) {
         List<Integer> result = new ArrayList<>();
+
         if (head != null) {
             Stack<Node> stack = new Stack<>();
-            Node cur = null;
-            stack.push(cur);
+            Node cur = head;
+
+            while (!stack.isEmpty() || cur != null){
+                if (cur != null) {
+                    stack.push(cur);
+                    cur = cur.left;
+                } else {
+                    cur = stack.pop();
+                    result.add(cur.value);
+                    // After pop up, access right tree.
+                    cur = cur.right;
+                }
+            }
+        }
+        return result;
+    }
+
+    public List<Integer> postOrderTraverse(Node head) {
+        List<Integer> result = new ArrayList<>();
+
+        if (head != null) {
+            Stack<Node> stack = new Stack<>();
+
+            // remember the child when pop from stack, represent parent if pushing to stack. Initiate with null.
+            Node prev = null;
+
+            stack.push(head);
 
             while (!stack.isEmpty()){
-                cur = stack.peek();
+                Node cur = stack.peek();
 
-                while (cur.left != null) {
-                    stack.push(cur.left);
-                    cur = cur.left;
+                if (prev == null || prev.left == cur ||  prev.right == cur) {
+                    /**
+                     * if it's root or current is prev's child, push child to stack. Otherwise to pop top node.
+                     */
+                    if (cur.left != null) {
+                        stack.push(cur.left);
+                    } else if (cur.right != null) {
+                        stack.push(cur.right);
+                    } else {
+                        result.add(cur.value);
+                        prev = stack.pop();
+                    }
+                } else if (cur.left == prev)   {
+                    // Once pop, if popped node is left child, check if top node has right child, pop top node if it
+                    // doesn't, otherwise push right child to stack.
+                    if (cur.right != null) {
+                        stack.push(cur.right);
+                    } else {
+                        stack.pop();
+                        result.add(cur.value);
+                    }
+                } else {
+                    // Once pop, if popped node is right child, just pop top node
+                    stack.pop();
+                    result.add(cur.value);
                 }
-
-                cur = stack.pop();
-                result.add(cur.value);
-
-                if (cur.right != null) {
-                    stack.push(cur.left);
-                    cur = cur.left;
-                }
+                prev = cur;
             }
         }
         return result;
--- java/src/test/java/com/haibo/yan/algorithm/tree/TestTraversal.java
+++ java/src/test/java/com/haibo/yan/algorithm/tree/TestTraversal.java
@@ -3,17 +3,58 @@ package com.haibo.yan.algorithm.tree;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.util.Arrays;
 import java.util.List;
+
+import static org.testng.Assert.assertEquals;
 
 @Test
 public class TestTraversal {
+    /**
+     *            9
+     *        /      \
+     *       4        13
+     *     /   \     /   \
+     *    3     6   10   15
+     *   /     /  \      /
+     *  1     5   7     14
+     *             \
+     *             8
+     * @return
+     */
     @DataProvider
-    public static Object[][] tree() {
-        return new Object[][] {{2, true}, {6, false}, {19, true}, {22, false}, {23, true}};
+    public static Object[][] preOrderTree() {
+        return new Object[][] {{Node.deserialize("9!4!3!1!###6!5!##7!#8!##13!10!##15!14!###"),
+                Arrays.asList(9, 4, 3, 1, 6, 5, 7, 8, 13, 10, 15, 14)}};
     }
 
+    @Test(dataProvider = "preOrderTree")
     public void testPreOrder(Node head, List<Integer> expectedResult){
-
+        Traversal traversal = new Traversal();
+        assertEquals(traversal.preOrderTraverse(head), expectedResult);
     }
 
+    @DataProvider
+    public static Object[][] inOrderTree() {
+        return new Object[][] {{Node.deserialize("9!4!3!1!###6!5!##7!#8!##13!10!##15!14!###"),
+                Arrays.asList(1, 3, 4, 5, 6, 7, 8, 9, 10, 13, 14, 15)}};
+    }
+
+    @Test(dataProvider = "inOrderTree")
+    public void testInOrder(Node head, List<Integer> expectedResult){
+        Traversal traversal = new Traversal();
+        assertEquals(traversal.inOrderTraverse(head), expectedResult);
+    }
+
+    @DataProvider
+    public static Object[][] postOrderTree() {
+        return new Object[][] {{Node.deserialize("9!4!3!1!###6!5!##7!#8!##13!10!##15!14!###"),
+                Arrays.asList(1, 3, 5, 8, 7, 6, 4, 10, 14, 15, 13, 9)}};
+    }
+
+    @Test(dataProvider = "postOrderTree")
+    public void testPostOrder(Node head, List<Integer> expectedResult){
+        Traversal traversal = new Traversal();
+        assertEquals(traversal.postOrderTraverse(head), expectedResult);
+    }
 }
