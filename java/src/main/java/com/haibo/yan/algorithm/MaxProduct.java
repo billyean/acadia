c8cce7b07fdbf09b9dfb5023ee8269393f6d5964
billyean <tristan.yim@gmail.com>
2017-09-25 20:48:36 -0700

    Move MaxProduct

--- java/src/main/java/com/haibo/yan/algorithm/MaxProduct.java
+++ java/src/main/java/com/haibo/yan/algorithm/MaxProduct.java
@@ -1,9 +1,25 @@
+/*
+ * Copyright [2017] [Haibo(Tristan) Yan]
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.haibo.yan.algorithm;
 
 /**
  * Created by hyan on 2/7/17.
  */
-public class Solution {
+public class MaxProduct {
     public int maxProduct(String[] words) {
         int[] iw = new int[words.length];
         int[] lw = new int[words.length];
@@ -37,6 +53,6 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        System.out.println(new Solution().maxProduct(new String[]{"abcw","baz","foo","bar","xtfn","abcdef"}));
+        System.out.println(new MaxProduct().maxProduct(new String[]{"abcw","baz","foo","bar","xtfn","abcdef"}));
     }
 }
