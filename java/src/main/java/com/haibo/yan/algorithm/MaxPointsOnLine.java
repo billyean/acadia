506053ea4ffc7e7c6d44f3ec063cae6e82fa259f
billyean <tristan.yim@gmail.com>
2017-10-18 11:14:27 -0700

    Add Max Points On a Line

--- java/src/main/java/com/haibo/yan/algorithm/MaxPointsOnLine.java
+++ java/src/main/java/com/haibo/yan/algorithm/MaxPointsOnLine.java
@@ -18,7 +18,11 @@ package com.haibo.yan.algorithm;
 
 import java.util.HashMap;
 
-
+/**
+ * see https://leetcode.com/problems/max-points-on-a-line
+ *
+ * Given n points on a 2D plane, find the maximum number of points that lie on the same straight line.
+ */
 class Point {
     int x;
     int y;
