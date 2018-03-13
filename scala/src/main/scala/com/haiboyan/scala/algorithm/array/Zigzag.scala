0a95726263d8aa9dad85cfd65f6d4927ebd5522f
Haibo Yan <hyan@cloudera.com>
2018-03-12 19:42:52 -0700

    [Scala] Zigzag

--- scala/src/main/scala/com/haiboyan/scala/algorithm/array/Zigzag.scala
+++ scala/src/main/scala/com/haiboyan/scala/algorithm/array/Zigzag.scala
@@ -7,16 +7,15 @@ object Zigzag {
     } else {
       var zs = ""
 
-      for (i <- 0 to s.length) {
-        var sp1 = 2 * (numRows - i - 1)
-        var sp2 = 2 * i
+      for (i <- 0 until numRows) {
+        val sp1 = 2 * (numRows - i - 1)
+        val sp2 = 2 * i
 
         var add = true
-
         var b = i
 
         while (b < s.length) {
-
+          zs += s(b)
           (sp1, sp2, add) match {
             case (0, _, _) => b += sp2
             case (_, 0, _) => b += sp1
