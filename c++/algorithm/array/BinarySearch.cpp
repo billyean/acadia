877c174dbc53acd5c4cacd299e786941afb00fd7
Haibo Yan <tristan.yim@gmail.com>
2018-03-19 19:16:03 -0700

    [C++] Binary Search

--- c++/algorithm/array/BinarySearch.cpp
+++ c++/algorithm/array/BinarySearch.cpp
@@ -4,25 +4,29 @@
 
 #include "BinarySearch.h"
 
+#include <iostream>
+
+using namespace std;
+
 int BinarySearch::search(int *rotated, int len, int k) {
     search(rotated, k, 0, len);
 }
 
 int BinarySearch::search(int *rotated, int k, int s, int e) {
-    if (s < e) {
+    if (s > e) {
         return -1;
     }
 
     int m = (s + e) >> 1;
 
     if (k < rotated[m]) {
-        if (rotated[s] < rotated[m] && k <= rotated[e - 1]) {
+        if (rotated[s] < rotated[m] && k >= rotated[e - 1] && k > rotated[s]) {
             search(rotated, k, m + 1, e);
         } else {
             search(rotated, k, s, m - 1);
         }
     } else if (k > rotated[m]) {
-        if (rotated[e - 1] > rotated[m] && k >= rotated[s]) {
+        if (rotated[e - 1] > rotated[m] && k < rotated[s]) {
             search(rotated, k, s, m - 1);
         } else {
             search(rotated, k, m + 1, e);
--- c++/algorithm_tests/array/BinarySearchTest.cpp
+++ c++/algorithm_tests/array/BinarySearchTest.cpp
@@ -11,3 +11,34 @@ TEST(BinarySearchTest, searchInOrdered) {
     ASSERT_EQ(BinarySearch::search(rotated, 7, 3), 2);
 }
 
+TEST(BinarySearchTest, searchInRotatedLeft) {
+    int rotated[] = {7, 1, 2, 3, 4, 5, 6};
+
+    ASSERT_EQ(BinarySearch::search(rotated, 7, 3), 3);
+}
+
+TEST(BinarySearchTest, searchInRotatedRight) {
+    int rotated[] = {3, 4, 5, 6, 7, 1, 2};
+
+    ASSERT_EQ(BinarySearch::search(rotated, 7, 3), 0);
+}
+
+TEST(BinarySearchTest, searchInOrderedWithEqual) {
+    int rotated[] = {1, 2, 3, 3, 4, 5, 5, 6, 6, 7};
+
+    ASSERT_EQ(BinarySearch::search(rotated, 10, 3), 2);
+}
+
+TEST(BinarySearchTest, searchInRotatedLeftWithEqual) {
+    int rotated[] = {6, 7, 1, 2, 3, 3, 4, 5, 5, 6};
+
+    ASSERT_EQ(BinarySearch::search(rotated, 10, 3), 4);
+}
+
+TEST(BinarySearchTest, searchInRotatedRightWithEqual) {
+    int rotated[] = {3, 4, 5, 5, 6, 6, 7, 1, 2, 3};
+
+    ASSERT_EQ(BinarySearch::search(rotated, 10, 3), 0);
+}
+
+
