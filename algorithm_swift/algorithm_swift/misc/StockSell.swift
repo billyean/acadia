a1330dbfb7a09e1dd6eca2fd2916ca59d21ad4f1
Haibo Yan <hyan@cloudera.com>
2018-03-26 10:46:05 -0700

    Fix README

---
+++ algorithm_swift/algorithm_swift/misc/StockSell.swift
@@ -0,0 +1,13 @@
+//
+//  StockSell.swift
+//  algorithm_swift
+//
+//  Created by Haibo Yan on 3/26/18.
+//  Copyright © 2018 Haibo Yan. All rights reserved.
+//
+
+import UIKit
+
+class StockSell: NSObject {
+
+}
