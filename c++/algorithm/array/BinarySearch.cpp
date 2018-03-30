369f9ef93408d27b874b4087399f7d7cca626e71
Haibo Yan <hyan@cloudera.com>
2018-03-30 12:06:04 -0700

    [C++] Set C++ standard as C++ 17

--- c++/algorithm/array/BinarySearch.cpp
+++ c++/algorithm/array/BinarySearch.cpp
@@ -9,7 +9,7 @@
 using namespace std;
 
 int BinarySearch::search(int *rotated, int len, int k) {
-    search(rotated, k, 0, len);
+    return search(rotated, k, 0, len);
 }
 
 int BinarySearch::search(int *rotated, int k, int s, int e) {
@@ -21,15 +21,15 @@ int BinarySearch::search(int *rotated, int k, int s, int e) {
 
     if (k < rotated[m]) {
         if (rotated[s] < rotated[m] && k >= rotated[e - 1] && k > rotated[s]) {
-            search(rotated, k, m + 1, e);
+            return search(rotated, k, m + 1, e);
         } else {
-            search(rotated, k, s, m - 1);
+            return search(rotated, k, s, m - 1);
         }
     } else if (k > rotated[m]) {
         if (rotated[e - 1] > rotated[m] && k < rotated[s]) {
-            search(rotated, k, s, m - 1);
+            return search(rotated, k, s, m - 1);
         } else {
-            search(rotated, k, m + 1, e);
+            return search(rotated, k, m + 1, e);
         }
     } else {
         while (m - 1 > 0 && rotated[m - 1] == k) {
