390ed65f772bf70d85b8497f1ad3b83e9d6cda7f
Haibo Yan <hyan@cloudera.com>
2018-04-05 12:28:48 -0700

    [Go] Print Pattern

---
+++ go/src/array/print_pattern.go
@@ -0,0 +1,29 @@
+package array
+
+func PrintPattern(n int) [][]int {
+	var pattern [][]int
+
+	if n == 0 {
+		return pattern
+	}
+	pattern = append(pattern, []int{1})
+
+	i := 0
+	for i + 1 < n {
+		next := []int{}
+		prev := pattern[i]
+		for j := 0; j < len(prev); {
+			n := j
+			j++
+			for j < len(prev) && prev[j] == prev[j - 1] {
+				j++
+			}
+			next = append(next, j - n)
+			next = append(next, prev[n])
+		}
+		pattern = append(pattern, next)
+		i++
+	}
+
+	return pattern
+}
