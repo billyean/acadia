b5eb71c1e797ca64162512d80071bdb9ac26d4d6
Haibo Yan <tristan.yim@gmail.com>
2018-04-10 00:57:27 -0700

    [C++] Largest Number

--- README.md
+++ README.md
@@ -45,6 +45,7 @@
 | Word Search                                      ||||||[`Solution`](scala/src/main/scala/com/haiboyan/scala/algorithm/array/WordSearch.scala)<br>[`Test`](scala/src/test/scala/com/haiboyan/scala/algorithm/array/WordSearchTest.scala)||
 | Intersection of Arrays                           |[`Solution`](java/src/main/java/com/haibo/yan/algorithm/array/IntersectionOfArray.scala)<br>~~`Test`~~|[`Solution`](python/parray/intersection.py)<br>[`Test`](python/test/parray/test_intersection.py)||||[`Solution`](scala/src/main/scala/com/haiboyan/scala/algorithm/array/Intersection.scala)<br>[`Test`](scala/src/test/scala/com/haiboyan/scala/algorithm/array/IntersectionTest.scala)||
 | Longest Consecutive Sequence                     |||[`Solution`](c++/algorithm/array/Sequence.cpp)<br>[`Test`](c++/algorithm_tests/array/SequenceTest.cpp)|||||
+| Largest Number                                   |||[`Solution`](c++/algorithm/array/LargestNumbder.cpp)<br>[`Test`](c++/algorithm_tests/array/LargestNumbderTest.cpp)|||||
 
 ### Matrix
 
---
+++ c++/algorithm/array/LargestNumber.cpp
@@ -0,0 +1,37 @@
+//Copyright [2018] [Haibo Yan]
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
+//
+// Created by Haibo Yan on 4/10/18.
+// Given a list of non negative integers, arrange them such that they form the largest number.
+//
+// For example, given [3, 30, 34, 5, 9], the largest formed number is 9534330. (Note: The result may be very large, so
+// you need to return a string instead of an integer.)
+//
+
+#include "LargestNumber.h"
+#include <algorithm>
+
+// 91910 91 9191910
+// 9191991 91 919199191
+string LargestNumber::largest(vector<string>& nums) {
+    sort(nums.begin(), nums.end(), [](const string& s1, const string& s2) -> bool {
+       return s2 + s1 < s1 + s2;
+    });
+
+    string ln = "";
+    for (auto it = nums.begin(); it != nums.end(); it++) {
+        ln += *it;
+    }
+    return ln;
+}
