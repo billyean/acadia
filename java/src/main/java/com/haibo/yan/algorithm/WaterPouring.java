8a82283e73218bb70c4236d4da76b45eef988f38
Haibo Yan <tristan.yim@gmail.com>
2018-06-08 02:11:08 -0700

    [Java] Fix Pouring water bugs

--- java/src/main/java/com/haibo/yan/algorithm/WaterPouring.java
+++ java/src/main/java/com/haibo/yan/algorithm/WaterPouring.java
@@ -26,58 +26,77 @@ public class WaterPouring {
 
 
     public void pouringPositions(int[] array, int waterAmount, int pos) {
-        int d = 0;
+        boolean leftNotReach = true;
+        boolean rightNotReach = true;
+
         if (pos < 0 && pos >= array.length) return;
 
         int n = array.length;
 
-        int[] poured = new int[array.length];
-
         int t = 0;
 
-        while (t < waterAmount && d < 2) {
-            int p = pos;
-
-            if (p + 1 < n && array[p + 1] <= array[p] ) {
-                while (p + 1 < n && array[p + 1] <= array[p]) {
-                    p++;
-                }
-                if (p != n - 1) {
-                    while (array[p] == array[p - 1]) {
-                        p--;
+        while (t < waterAmount && (leftNotReach || rightNotReach)) {
+            boolean tryLeft = false;
+            if (rightNotReach) {
+                int right = fillOneBottom(array, pos, false);
+                if (right == array.length - 1 || array[right] == array[pos]) {
+                    if (right == array.length - 1) {
+                        rightNotReach = false;
                     }
-                    array[p]++;
+                    tryLeft = true;
                 } else {
-                    d++;
+                    array[right]++;
+                    t++;
                 }
-            } else if (p > 0 && array[p - 1] <= array[p]){
-                while (p > 0 && array[p - 1] <= array[p]) {
-                    while (array[p] == array[p + 1]) {
-                        p++;
-                    }
-                    p--;
-                }
-                if (p != 0) {
-                    array[p]++;
-                } else {
-                    d++;
-                }
-            } else {
-                while (p > 0 && array[p - 1] < array[p]) {
-                    p--;
-                }
-                if (p != 0) {
-                    array[p]++;
-                } else {
-                    d++;
-                }
-                array[p]++;
             }
 
-            if (p >= 0 && p < n) {
-                t++;
+            boolean tryRight = false;
+            if (tryLeft && leftNotReach) {
+                int left = fillOneBottom(array, pos, true);
+                if (left == 0 || array[left] == array[pos]) {
+                    if (left == 0) {
+                        leftNotReach = false;
+                    }
+                    tryRight = true;
+                } else {
+                    array[left]++;
+                    t++;
+                }
             }
 
+            if (tryRight && tryLeft) {
+                if (leftNotReach && rightNotReach) {
+                    array[pos]++;
+                    t++;
+                } else {
+                    leftNotReach = false;
+                    rightNotReach = false;
+                }
+            }
+        }
+    }
+
+    private int fillOneBottom(int[] array, int pos, boolean isLeft) {
+        int c = array[pos];
+        int n = pos;
+        if (isLeft) {
+            while (n - 1 >= 0 && array[n - 1] <= c) {
+                n--;
+                c = Math.min(array[n], c);
+            }
+            while (n > 0 && n < pos && array[n] == array[n + 1]) {
+                n++;
+            }
+            return n;
+        } else {
+            while (n + 1 < array.length && array[n + 1] <= c) {
+                n++;
+                c = Math.min(array[n], c);
+            }
+            while (n < array.length - 1 && n > pos && array[n] == array[n - 1]) {
+                n--;
+            }
+            return n;
         }
     }
 
@@ -101,19 +120,19 @@ public class WaterPouring {
         int[] filled3 = new int[bars3.length];
         System.arraycopy(bars3, 0, filled3, 0, filled3.length);
         new WaterPouring().pouringPositions(filled3, 30, 1);
-        new WaterPouring().printBar(bars1, filled3);
+        new WaterPouring().printBar(bars3, filled3);
 
         System.out.println("Testing 4....");
         int[] bars4 = new int[]{5, 4, 2, 2, 2, 3, 2, 1, 0, 1, 2, 4};
         int[] filled4 = new int[bars1.length];
-        System.arraycopy(bars1, 0, filled4, 0, filled4.length);
+        System.arraycopy(bars4, 0, filled4, 0, filled4.length);
         new WaterPouring().pouringPositions(filled4, 8, 3);
         new WaterPouring().printBar(bars4, filled4);
 
         System.out.println("Testing 5....");
         int[] bars5 = new int[]{5, 4, 2, 2, 2, 3, 2, 1, 0, 1, 2, 4};
         int[] filled5 = new int[bars1.length];
-        System.arraycopy(bars1, 0, filled5, 0, filled5.length);
+        System.arraycopy(bars5, 0, filled5, 0, filled5.length);
         new WaterPouring().pouringPositions(filled5, 11, 3);
         new WaterPouring().printBar(bars5, filled5);
 
@@ -123,5 +142,26 @@ public class WaterPouring {
         System.arraycopy(bars6, 0, filled6, 0, filled6.length);
         new WaterPouring().pouringPositions(filled6, 12, 3);
         new WaterPouring().printBar(bars6, filled6);
+
+        System.out.println("Testing 7....");
+        int[] bars7 = new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4};
+        int[] filled7 = new int[bars7.length];
+        System.arraycopy(bars7, 0, filled7, 0, filled7.length);
+        new WaterPouring().pouringPositions(filled7, 12, 3);
+        new WaterPouring().printBar(bars7, filled7);
+
+        System.out.println("Testing 8....");
+        int[] bars8 = new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+        int[] filled8 = new int[bars8.length];
+        System.arraycopy(bars8, 0, filled8, 0, filled8.length);
+        new WaterPouring().pouringPositions(filled8, 12, 3);
+        new WaterPouring().printBar(bars8, filled8);
+
+        System.out.println("Testing 9....");
+        int[] bars9 = new int[]{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 4};
+        int[] filled9 = new int[bars9.length];
+        System.arraycopy(bars9, 0, filled9, 0, filled9.length);
+        new WaterPouring().pouringPositions(filled9, 2, 3);
+        new WaterPouring().printBar(bars9, filled9);
     }
 }
