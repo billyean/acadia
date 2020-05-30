0644e654afafe2738d63103594ff1457fcaa4e10
Haibo Yan <tristan.yim@gmail.com>
2020-05-30 10:34:03 -0700

    Add one medium

--- java/src/main/java/com/haibo/yan/algorithm/tree/LargestInRow.java
+++ java/src/main/java/com/haibo/yan/algorithm/tree/LargestInRow.java
@@ -1,4 +1,42 @@
 package com.haibo.yan.algorithm.tree;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/find-largest-value-in-each-tree-row/
+ */
 public class LargestInRow {
+    public List<Integer> largestValues(BinaryTreeNode root) {
+        List<Integer> r = new ArrayList<>();
+        if (root == null) {
+            return r;
+        }
+
+        List<Integer> ll = largestValues(root.left);
+        List<Integer> lr = largestValues(root.right);
+
+        r.add(root.val);
+
+        if (ll.size() > lr.size()) {
+            List<Integer> t = ll;
+            ll = lr;
+            lr = t;
+        }
+
+        int i = 0;
+        while (i < ll.size()) {
+            if (ll.get(i) >= lr.get(i)) {
+                r.add(ll.get(i));
+            } else {
+                r.add(lr.get(i));
+            }
+            i++;
+        }
+        while (i < lr.size()) {
+            r.add(lr.get(i));
+            i++;
+        }
+        return r;
+    }
 }
