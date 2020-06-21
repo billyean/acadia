8f60e1112c14010d8e019f5b4e5f4d2c79d5f834
Haibo Yan <tristan.yim@gmail.com>
2020-06-20 21:26:44 -0700

    Add one medium

--- java/src/main/java/com/haibo/yan/algorithm/array/CombinationSum.java
+++ java/src/main/java/com/haibo/yan/algorithm/array/CombinationSum.java
@@ -1,4 +1,12 @@
 package com.haibo.yan.algorithm.array;
 
+import java.util.List;
+
 public class CombinationSum {
+//    public List<List<Integer>> combinationSum3(int k, int n) {
+//        List<Integer>[] level = new List[n];
+//
+//    }
+
+
 }
