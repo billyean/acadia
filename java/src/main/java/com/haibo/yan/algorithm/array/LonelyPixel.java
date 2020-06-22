c12bc83eda691fcd3d65aeab49c5e7640acfb124
Haibo Yan <tristan.yim@gmail.com>
2020-06-21 23:45:43 -0700

    One more medium

--- java/src/main/java/com/haibo/yan/algorithm/array/LonelyPixel.java
+++ java/src/main/java/com/haibo/yan/algorithm/array/LonelyPixel.java
@@ -1,5 +1,9 @@
 package com.haibo.yan.algorithm.array;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 public class LonelyPixel {
     /**
      * https://leetcode.com/problems/lonely-pixel-i/
@@ -36,6 +40,52 @@ public class LonelyPixel {
         }
 
         return sum;
+    }
 
+    /**
+     * https://leetcode.com/problems/lonely-pixel-ii/
+     * @param picture
+     * @param N
+     * @return
+     */
+    public int findBlackPixel(char[][] picture, int N) {
+        int r = picture.length;
+        int c = picture[0].length;
+
+
+        HashMap<String, Integer> bws = new HashMap<>();
+        List<String> rs = new ArrayList<>();
+        int[] rows = new int[r];
+        int[] cols = new int[c];
+
+
+        for (int i = 0; i < r; i++) {
+            StringBuilder b = new StringBuilder();
+            for (int j = 0; j< c; j++) {
+                if(picture[i][j] == 'B') {
+                    rows[i]++;
+                    cols[j]++;
+                }
+                b.append(picture[i][j]);
+            }
+
+            String s = b.toString();
+            rs.add(s);
+            bws.put(s, bws.getOrDefault(s, 0) + 1);
+        }
+
+        int res = 0;
+
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j< c; j++) {
+                if(picture[i][j] == 'B') {
+                    if (rows[i] == N && cols[j] == N && bws.get(rs.get(i)) == N) {
+                        res++;
+                    }
+                }
+            }
+        }
+
+        return res;
     }
 }
